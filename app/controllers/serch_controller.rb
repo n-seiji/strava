5cf9f3189608aafb68e8a613105ff5679143ae4e
中山聖司 <seiji@nakayamaseijinoMacBook-Pro-9.local>
2019-06-16 23:03:53 +0900

    ha

--- app/controllers/serch_controller.rb
+++ app/controllers/serch_controller.rb
@@ -2,6 +2,9 @@ class SerchController < ApplicationController
   protect_from_forgery :except => [:new]
   def new
     baespots = Baespot.all
-    @baespots = baespots.sample
+    baespots = baespots.shuffle
+    @baespots_1 = baespots[0]
+    @baespots_2 = baespots[1]
+    @baespots_3 = baespots[2]
   end
 end
